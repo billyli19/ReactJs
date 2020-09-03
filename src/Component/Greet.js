0f69107859c40702608ef45c7fad0ebadf2b963d
billyli19 <billy1999fans@gmail.com>
2020-09-03 12:20:48 +0200

    Functional Component added

---
+++ src/Component/Greet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// function Greet() {
+//   return
+//     <h1>
+//       Hello Billy
+//     </h1>;
+// }
+
+//ES6 syntax
+const Greet = () => <h1> Hello Billy</h1>;
+
+export default Greet;
