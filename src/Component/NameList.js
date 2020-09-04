911951edc43deaab6a0eccfeb8a1109af42fe113
billyli19 <billy1999fans@gmail.com>
2020-09-04 10:44:14 +0200

    List Rendering Component modified

--- src/Component/NameList.js
+++ src/Component/NameList.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Person from "./Person";
 
 function NameList() {
-  const names = ["Bruce", "Clark", "Diana"];
-  return (
-    <div>
-      {names.map((name) => (
-        <h2>{name}</h2>
-      ))}
-    </div>
-  );
+  const persons = [
+    {
+      id: 1,
+      name: "Bruce",
+      age: 30,
+      heroName: "Batman",
+    },
+    {
+      id: 2,
+      name: "Clark",
+      age: 32,
+      heroName: "Superman",
+    },
+    {
+      id: 3,
+      name: "Diana",
+      age: 31,
+      heroName: "Wonderwoman",
+    },
+  ];
+  const personList = persons.map((person) => <Person person={person} />);
+  return <div>{personList}</div>;
 }
 
 export default NameList;
